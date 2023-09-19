46456119788a31573aeb001764c9bb212fe28994
sagarp4653 <97823092+sagarp4653@users.noreply.github.com>
2023-09-19 13:02:02 +0530

    Part 3: Failing test case

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -15,7 +15,7 @@ class RestaurantTest {
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.openMocks(this);
-        
+
         // Create a restaurant with a menu before each test
         LocalTime openingTime = LocalTime.parse("10:30:00");
         LocalTime closingTime = LocalTime.parse("22:00:00");
@@ -35,5 +35,25 @@ class RestaurantTest {
     public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time() {
         LocalTime currentTime = LocalTime.parse("23:30:00");
         assertFalse(restaurant.isRestaurantOpen(currentTime));
+    }
+
+    // MENU TEST CASES
+    @Test
+    public void adding_item_to_menu_should_increase_menu_size_by_1() {
+        int initialMenuSize = restaurant.getMenu().size();
+        restaurant.addToMenu("Sizzling brownie", 319);
+        assertEquals(initialMenuSize + 1, restaurant.getMenu().size());
+    }
+
+    @Test
+    public void removing_item_from_menu_should_decrease_menu_size_by_1() throws itemNotFoundException {
+        int initialMenuSize = restaurant.getMenu().size();
+        restaurant.removeFromMenu("Vegetable lasagne");
+        assertEquals(initialMenuSize - 1, restaurant.getMenu().size());
+    }
+
+    @Test
+    public void removing_item_that_does_not_exist_should_throw_exception() {
+        assertThrows(itemNotFoundException.class, () -> restaurant.removeFromMenu("French fries"));
     }
 }
